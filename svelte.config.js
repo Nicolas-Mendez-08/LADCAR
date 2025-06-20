a037ed507022ef796f7cc2e6c5f0c5db02f3991d
Nicolas-Mendez-08 <desarrollonicolasmendez@gmail.com>
2025-06-20 03:57:10 -0700

    Configurado adaptador de Netlify para SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 
-const config = { kit: { adapter: adapter() } };
-
-export default config;
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
